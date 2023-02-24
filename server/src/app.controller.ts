f99e1478be8661520cbbb1f7ed916cad5d63b9d8
kosuke-17 <tamutamu17@icloud.com>
2023-02-25 08:18:52 +0900

    create login dto

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,11 +1,23 @@
-import { Controller, Post, UseGuards, Req } from '@nestjs/common'
-import { AuthGuard } from '@nestjs/passport'
+import {
+  Controller,
+  Post,
+  UseGuards,
+  Req,
+  HttpStatus,
+  HttpCode,
+} from '@nestjs/common'
+import { ApiBody } from '@nestjs/swagger'
 import { Request } from 'express'
+
+import { LocalAuthGuard } from 'src/auth/local-auth.guard'
+import { LoginDto } from 'src/users/dtos/login.dto'
 
 @Controller()
 export class AppController {
-  @UseGuards(AuthGuard('local'))
+  @UseGuards(LocalAuthGuard)
   @Post('auth/login')
+  @HttpCode(HttpStatus.OK)
+  @ApiBody({ type: LoginDto })
   async login(@Req() req: Request) {
     return req.user
   }

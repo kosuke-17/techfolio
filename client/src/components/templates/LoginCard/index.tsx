0524c9a1416ed1e955527958809cb8cd1afa8fc1
kosuke-17 <tamutamu17@icloud.com>
2023-02-28 08:00:44 +0900

    create login card

--- client/src/components/templates/LoginCard/index.tsx
+++ client/src/components/templates/LoginCard/index.tsx
@@ -1,13 +1,25 @@
+import Button from '@mui/material/Button'
 import Card from '@mui/material/Card'
 import CardContent from '@mui/material/CardContent'
+import CardActions from '@mui/material/CardActions'
 import { styled } from '@mui/material/styles'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
 
-import { getUsers } from '@/store/hooks/user'
-import { Button, CardActions } from '@mui/material'
+import CustomTextField from '@/components/presentations/CustomTextField'
+import Stack from '@mui/material/Stack'
+
+const loginSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(1),
+})
+export type DefaultValues = z.infer<typeof loginSchema>
 
 const StyledCard = styled(Card)(({ theme }) => ({
   width: 400,
   borderRadius: theme.shape.borderRadius,
+  padding: theme.spacing(1),
 }))
 
 const StyledCardContent = styled(CardContent)(({}) => ({
@@ -17,12 +29,40 @@ const StyledCardContent = styled(CardContent)(({}) => ({
 }))
 
 const Login = () => {
-  const users = getUsers()
-  console.log(users)
+  const login = () => {
+    console.log('ログイン')
+  }
+
+  const defaultValues = {
+    email: '',
+    password: '',
+  }
+
+  const { control, handleSubmit } = useForm<DefaultValues>({
+    resolver: zodResolver(loginSchema),
+    defaultValues,
+  })
 
   return (
     <StyledCard>
-      <StyledCardContent>カードの中身</StyledCardContent>
+      <StyledCardContent>
+        <form onSubmit={handleSubmit(login)}>
+          <Stack spacing={2}>
+            <CustomTextField
+              name='email'
+              label='メール'
+              control={control}
+              defaultValue=''
+            />
+            <CustomTextField
+              name='password'
+              label='パスワード'
+              control={control}
+              defaultValue=''
+            />
+          </Stack>
+        </form>
+      </StyledCardContent>
       <CardActions sx={{ display: 'flex', justifyContent: 'center' }}>
         <Button sx={{ width: '50%' }} variant='contained'>
           ログイン

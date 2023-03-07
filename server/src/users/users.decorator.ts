4ca538124533dad3387789370f3ded9fe626be00
kosuke-17 <tamutamu17@icloud.com>
2023-03-07 23:07:39 +0900

    delete

--- server/src/users/users.decorator.ts
+++ server/src/users/users.decorator.ts
@@ -2,7 +2,5 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common'
 
 export const LoginUser = createParamDecorator((_, ctx: ExecutionContext) => {
   const request = ctx.switchToHttp().getRequest()
-  console.log(request)
-
   return request.user
 })

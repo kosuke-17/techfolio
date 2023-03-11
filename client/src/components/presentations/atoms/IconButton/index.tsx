28b69310884e94a960533dd9535b4e13d78ce98f
kosuke-17 <tamutamu17@icloud.com>
2023-03-11 17:51:48 +0900

    create IconButton

---
+++ client/src/components/presentations/atoms/IconButton/index.tsx
@@ -0,0 +1,18 @@
+import { SvgIconComponent } from '@mui/icons-material'
+import { IconButton as MUIIconButton, SxProps } from '@mui/material'
+
+const IconButton = ({
+  icon: Icon,
+  iconSx,
+}: {
+  icon: SvgIconComponent
+  iconSx: SxProps
+}) => {
+  return (
+    <MUIIconButton sx={{ boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)' }}>
+      <Icon sx={iconSx} />
+    </MUIIconButton>
+  )
+}
+
+export default IconButton
--- client/src/components/templates/SpreadSheet/index.tsx
+++ client/src/components/templates/SpreadSheet/index.tsx
@@ -1,13 +1,13 @@
 import Box from '@mui/material/Box'
 import Paper from '@mui/material/Paper'
 import React from 'react'
-import IconButton from '@mui/material/IconButton'
 import Typography from '@mui/material/Typography'
 import CloudDownloadIcon from '@mui/icons-material/CloudDownload'
 import { styled } from '@mui/material/styles'
 
 import ContentCenter from '@/components/presentations/ContentCenter'
 import UserInformation from '@/components/UserInformation'
+import IconButton from '@/components/presentations/atoms/IconButton'
 
 const StyledSpreadSheetHeader = styled(Box)(({ theme }) => ({
   paddingTop: theme.spacing(3),
@@ -21,11 +21,10 @@ const SpreadSheet = () => {
       <Paper sx={{ borderRadius: 10, px: 3 }} elevation={3}>
         <StyledSpreadSheetHeader>
           <Typography variant='h3'>SpreadSheet</Typography>
-          <IconButton sx={{ boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)' }}>
-            <CloudDownloadIcon
-              sx={{ color: 'common.black', fontSize: '32px' }}
-            />
-          </IconButton>
+          <IconButton
+            icon={CloudDownloadIcon}
+            iconSx={{ color: 'common.black', fontSize: '32px' }}
+          />
         </StyledSpreadSheetHeader>
 
         <UserInformation />

2be056ee1e414d4a13071fd9fc708fd73b2766c3
kosuke-17 <tamutamu17@icloud.com>
2023-03-19 14:46:06 +0900

    create editpage tabpanel

---
+++ client/src/components/presentations/atoms/TabPanel/index.tsx
@@ -0,0 +1,35 @@
+import { FC } from 'react'
+import Box from '@mui/material/Box'
+import Stack from '@mui/material/Stack'
+import { styled } from '@mui/material/styles'
+import type { TabType } from '@/components/templates/SpreadSheetEditForm/hooks'
+
+type Props = {
+  children?: React.ReactNode
+  tabType: TabType
+  value: TabType
+}
+
+const StyledChildrenStack = styled(Stack)(({ theme }) => ({
+  paddingTop: theme.spacing(3),
+  paddingBottom: theme.spacing(3),
+  paddingLeft: theme.spacing(3),
+  paddingRight: theme.spacing(3),
+}))
+
+const TabPanel: FC<Props> = ({ children, value, tabType }) => {
+  return (
+    <Box
+      role='tabpanel'
+      hidden={value !== tabType}
+      id={tabType}
+      aria-labelledby={tabType}
+    >
+      {value === tabType && (
+        <StyledChildrenStack spacing={2}>{children}</StyledChildrenStack>
+      )}
+    </Box>
+  )
+}
+
+export default TabPanel
--- client/src/components/templates/SpreadSheetEditForm/index.tsx
+++ client/src/components/templates/SpreadSheetEditForm/index.tsx
@@ -1,23 +1,51 @@
 import Box from '@mui/material/Box'
 import Paper from '@mui/material/Paper'
-import React from 'react'
 import Typography from '@mui/material/Typography'
+import Tabs from '@mui/material/Tabs'
+import Tab from '@mui/material/Tab'
 import { styled } from '@mui/material/styles'
 
 import ContentCenter from '@/components/presentations/ContentCenter'
+import TabPanel from '@/components/presentations/atoms/TabPanel'
+import { useHooks } from './hooks'
 
 const StyledSpreadSheetHeader = styled(Box)(({ theme }) => ({
   display: 'flex',
   justifyContent: 'space-between',
 }))
 
+const StyledTabLabelBox = styled(Box)(() => ({
+  fontSize: '16px',
+  fontWeight: 700,
+}))
+
 const SpreadSheetEditForm = () => {
+  const { value, handleChange } = useHooks()
+
+  // TODO:labelはバックエンドから渡したい
+  const tabs = [
+    { label: '基本情報', value: 'info' },
+    { label: 'ポートフォリオ', value: 'portfolio' },
+    { label: 'スキル要約', value: 'skill' },
+  ]
+
   return (
     <ContentCenter sx={{ py: 3 }}>
       <Paper sx={{ py: 3, borderRadius: 10, px: 3 }} elevation={3}>
         <StyledSpreadSheetHeader>
           <Typography variant='h5'>SpreadSheetの編集</Typography>
         </StyledSpreadSheetHeader>
+        <Tabs value={value} onChange={handleChange}>
+          {tabs.map(({ label, value }) => (
+            <Tab
+              key={label}
+              label={<StyledTabLabelBox>{label}</StyledTabLabelBox>}
+              value={value}
+              sx={{ marginLeft: 0.5 }}
+            />
+          ))}
+        </Tabs>
+        <TabPanel value={value} tabType='info'></TabPanel>
       </Paper>
     </ContentCenter>
   )

f2f2676133e4ff8c5268d6785262cd504618cffd
kosuke-17 <tamutamu17@icloud.com>
2023-03-28 08:12:58 +0900

    基本情報を取得して表示

--- client/src/components/templates/UserInformation/hooks.ts
+++ client/src/components/templates/UserInformation/hooks.ts
@@ -1,21 +1,45 @@
 import { useRouter } from 'next/router'
 
 import type { TabType } from '@/components/templates/SpreadSheetEditForm/hooks'
+import { useMe } from '@/hooks/api/user'
+import { useUserInformation } from '@/hooks/api/user-information'
+import { GENDER } from '@/constant/user-information'
+import { useMemo } from 'react'
 
 type Props = { tabType: TabType }
+type RowType = { name: string; content: string }
 
 export const useHooks = ({ tabType }: Props) => {
   const router = useRouter()
+  const { me } = useMe()
+  const { userInformation } = useUserInformation({ id: me?.userInformation.id })
+
+  const rows = useMemo(() => {
+    if (!userInformation) return []
+    const {
+      stuffId,
+      age,
+      gender,
+      nearestStation,
+      startWorkDate,
+      seExpAmount,
+      pgExpAmount,
+      itExpAmount,
+    } = userInformation
+    return [
+      { name: 'スタッフID', content: stuffId },
+      { name: '年齢', content: age.toString() },
+      { name: '性別', content: GENDER[gender].LABEL },
+      { name: '最寄駅', content: nearestStation },
+      { name: '稼働開始日', content: startWorkDate },
+      { name: 'SE経験', content: seExpAmount.toString() },
+      { name: 'PG・作業員経験', content: pgExpAmount.toString() },
+      { name: 'IT全体経験', content: itExpAmount.toString() },
+    ]
+  }, [userInformation])
+
   const colums = [{ name: '項目名' }, { name: '内容' }]
-  const rows = [
-    { name: 'スタッフID', content: 'FR-204-2442' },
-    { name: '年齢', content: '20代前半' },
-    { name: '性別', content: '男' },
-    { name: '最寄駅', content: '西武新宿線下落合駅' },
-    { name: '稼働開始日', content: '応相談' },
-    { name: 'エンジニア歴', content: '1年5ヶ月' },
-    { name: 'IT全体歴', content: '1年8ヶ月' },
-  ]
+
   const goToEdit = () => {
     router.push({
       pathname: '/spread-sheet/edit',

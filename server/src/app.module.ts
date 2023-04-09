7b85426e692f14755362ae775c7287d6f265453d
kosuke-17 <tamutamu17@icloud.com>
2023-04-09 15:20:28 +0900

    portfolioのファイル作成

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -5,9 +5,10 @@ import { UsersModule } from './users/users.module'
 import { LoggerService } from './logger/logger.service'
 import { AuthModule } from './auth/auth.module'
 import { UserInformationsModule } from './user-informations/user-informations.module'
+import { PortfoliosModule } from './portfolios/portfolios.module';
 
 @Module({
-  imports: [PrismaModule, UsersModule, AuthModule, UserInformationsModule],
+  imports: [PrismaModule, UsersModule, AuthModule, UserInformationsModule, PortfoliosModule],
   controllers: [AppController],
   providers: [LoggerService],
 })

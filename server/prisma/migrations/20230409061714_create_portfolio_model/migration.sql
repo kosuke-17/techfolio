44ee1ef7769a81a95b1f92beff0e3116684e1c53
kosuke-17 <tamutamu17@icloud.com>
2023-04-09 15:18:30 +0900

    portfolioモデル作成

---
+++ server/prisma/migrations/20230409061714_create_portfolio_model/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE "Portfolio" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "userId" TEXT NOT NULL,
+    "name" TEXT,
+    "url" TEXT,
+
+    CONSTRAINT "Portfolio_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Portfolio_userId_key" ON "Portfolio"("userId");
+
+-- AddForeignKey
+ALTER TABLE "Portfolio" ADD CONSTRAINT "Portfolio_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;

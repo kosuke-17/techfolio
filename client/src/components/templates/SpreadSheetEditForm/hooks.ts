5feb71f2b88ae960f1f18967093791a964ff2ae1
kosuke-17 <tamutamu17@icloud.com>
2023-04-09 15:11:10 +0900

    fix

--- client/src/components/templates/SpreadSheetEditForm/hooks.ts
+++ client/src/components/templates/SpreadSheetEditForm/hooks.ts
@@ -1,12 +1,13 @@
+import { TAB_TYPE } from '@/constant/spreadsheet'
 import { useSpreadSheet } from '@/hooks/spreadSheet'
 
 export const useHooks = () => {
   const { tabType, goToBack, onChangeTabType } = useSpreadSheet()
   // TODO:labelはバックエンドから渡したい
   const tabs = [
-    { label: '基本情報', value: 'info' },
-    { label: 'ポートフォリオ', value: 'portfolio' },
-    { label: 'スキル要約', value: 'skill' },
+    { label: '基本情報', value: TAB_TYPE.Info },
+    { label: 'ポートフォリオ', value: TAB_TYPE.Portfolio },
+    { label: 'スキル要約', value: TAB_TYPE.Skill },
   ]
 
   return {
